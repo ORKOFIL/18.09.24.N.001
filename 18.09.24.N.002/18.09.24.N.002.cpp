d4a47c172806a098a123800b675be77e275e58d5
INASHOZE <ultimolviv2018@gmail.com>
2024-09-18 13:52:53 +0300

    updated "бульбашковие сортування" and added swaps and interations count.

--- 18.09.24.N.002/18.09.24.N.002.cpp
+++ 18.09.24.N.002/18.09.24.N.002.cpp
@@ -6,14 +6,28 @@ using namespace std;
 int main() {
 	const int length(9);
 	int array[length] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
+	int leng = length;
+	int swapcount = 0;
+	int count = 1;
+	int intercount = 0;
 	for (int first = 0; first < length; first++) {
-		for (int second = first + 1; second < length; second++) {
-			if (array[first] < array[second]) {
-				swap(array[first], array[second]);
+		int swaps = swapcount;
+		for (int second = 0; second < length-count; second++) {
+			if (array[second] > array[second + 1]) {
+				swap(array[second], array[second + 1]);
+				swapcount++;
 			}
 		}
+		intercount++;
+		count++;
+		if (swapcount == swaps) {
+			break;
+		}
 	}
+	cout << "total swaps: " << swapcount << endl;
+	cout << "total interations: " << intercount << endl;
 	for (int index = 0; index < length; index++) {
-		cout << array[index];
+		cout << array[index] << " ";
 	}
+	cout << endl;
 }
